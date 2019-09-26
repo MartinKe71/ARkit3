05bc18d15651477497273141aa76d291df8a876b
MartinKe71 <tk26@rice.edu>
2019-09-26 12:51:11 -0500

    Added BodyTracking Sample

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private int current;
+    private int total;
+    void Start()
+    {
+        current = SceneManager.GetActiveScene().buildIndex;
+        total = SceneManager.sceneCountInBuildSettings;
+    }
+
+    // Update is called once per frame
+    public void Prev()
+    {
+        if (current - 1 >= 0)
+        {
+            current--;
+            SceneManager.LoadScene(current);
+        }
+    }
+
+    public void Next()
+    {
+        if (current < total - 1)
+        {
+            current++;
+            SceneManager.LoadScene(current);
+        }
+    }
+}
